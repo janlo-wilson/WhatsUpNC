d35866c08c4bb0a18314d7a0ed96345d3c50d3d1
Jan Wilson <janlo.wilson@gmail.com>
2020-03-29 17:10:16 -0700

    Removed previous calendar, added new one, updated index page

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,3 @@ $("#submitEventButton").click(function() {
     $("#submitEventModal").modal("show");
 });
 
-let calendar = new CalendarYvv ("#calendar", moment().format("Y-M-D"), "Monday");
-         calendar.createCalendar();
-         calendar.diasResal = [1,2,3];
-         calendar.colorResal = "#28a7454d";
-         calendar.textResalt = "#28a745";
-         calendar.bg = "bg-dark";
-         calendar.textColor = "text-white";
-         calendar.btnH = "btn-outline-light";
-         calendar.btnD = "btn-rounded-success";
-         calendar.funcPer = function(ev){
-             console.log(ev)};
